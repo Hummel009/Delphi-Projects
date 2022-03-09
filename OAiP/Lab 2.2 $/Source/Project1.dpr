1ab79a3ab1c0faf79f4e26a20444735377897732
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-03-09 21:40:18 +0300

    Now it works well

--- OAiP/Lab 2.2 $/Source/Project1.dpr
+++ OAiP/Lab 2.2 $/Source/Project1.dpr
@@ -1,72 +1,72 @@
 Program Lab2;
-{Compare Sorts}
+{Compare sorts}
 
-//Use App
+//Use app
 {$APPTYPE CONSOLE}
 
-//Declare Modules
+//Declare modules
 Uses
   SysUtils;
 
-//Declare Types
+//Declare types
 Type
-  TArr=Array[0..3001] Of Integer;
-
+  TArr = Array[0..3000] Of Integer;
+  //TArr - our array
+  
 //Declare Vars
 Var
   I, J: Integer;
-  Nums: Array[1..6] Of Integer=(100, 250, 500, 1000, 2000, 3000);
-
-Procedure Swap(Var Arr, B: Integer);
+  Nums: Array[1..6] Of Integer = (100, 250, 500, 1000, 2000, 3000);
+  //I,J - loop params
+  //Nums - array sizes
+  
+Procedure Swap(Var A, B: Integer);
 Var
-  Temp: Integer;
+  T: Integer;
+  //T - temp
 Begin
-  Temp:=Arr;
-  Arr:=B;
-  B:=Temp;
+  T:= A;
+  A:= B;
+  B:= T;
 End;
 
-Procedure ArrS(Var Arr: TArr; Const N: Integer);
+Procedure Fill(Var Arr: TArr; Const N, Opt: Integer);
 Var
   I: Integer;
-Begin
-  For I:=1 To N Do
-    Arr[I]:=I;
-End;
-
-Procedure ArrVV(Var Arr: TArr; Const N: Integer);
-Var
-  I: Integer;
-Begin
-  For I:=1 To N Do
-    Arr[I]:=N-I;
-End;
-
-Procedure ArrR(Var Arr: TArr; Const N: Integer);
-Var
-  I: Integer;
+  //I - select type of filling
 Begin
   Randomize;
-  For I:=1 To N Do
-    Arr[I]:=Random(N);
+  For I:= 1 To N Do
+  Begin
+    Case Opt Of
+      1:
+        Arr[I]:= Random(N);
+      2:
+        Arr[I]:= I;
+      3:
+        Arr[I]:= N - I;
+    End;
+  End;
 End;
 
 Procedure BubbleSort(Var Arr: TArr; Const N: Integer; Var Comp: Integer);
 Var
   I, J: Integer;
   Sorted: Boolean;
+  //I, J - loop params
+  //Sorted - condition to exit
 Begin
-  Sorted:=False;
-  I:=1;
-  While (I<=N-1)And Not Sorted Do
+  Sorted:= False;
+  I:= 1;
+  While (I <= N - 1) And Not Sorted Do
   Begin
-    Sorted:=True;
-    For J:=1 To N-I Do
+    Sorted:= True;
+    For J:= 1 To N - I Do
     Begin
-      If Arr[J]>Arr[J+1] Then
+      If Arr[J] > Arr[J + 1] Then
       Begin
-        Sorted:=False;
-        Swap(Arr[J], Arr[J+1]);
+        Sorted:= False;
+        Swap(Arr[J], Arr[J + 1]);
       End;
       Inc(Comp);
     End;
@@ -74,100 +74,111 @@ Begin
   End;
 End;
 
-Procedure SiftDown(Var Arr: TArr; Var Node: Integer; Var NodeLast: Integer; Var Comp: Integer);
+Procedure SiftDown(Var Arr: TArr; Node, NodeLast: Integer; Var Comp: Integer);
 Var
-  Child: Integer;
+  Root, Child: Integer;
   Sifted: Boolean;
+  //Root, Child - indexes
+  //Sifted - condition to exit
 Begin
-  Sifted:=False;
-  Child:=2*Node;
-  While (Not Sifted)And(Child<=NodeLast) Do
+  Root:= Node;
+  Sifted:= False;
+  While (Not Sifted) And (Root * 2 - Node + 1 <= NodeLast) Do
   Begin
-    If (Child+1<NodeLast)And(Arr[Child]<Arr[Child+1]) Then
+    Comp:= Comp + 2;
+    Child:= Root * 2 - Node + 1;
+    If (Child + 1 <= NodeLast) And (Arr[Child] < Arr[Child + 1]) Then
       Inc(Child);
-    Comp:=Comp+2;
-    If (Arr[Child]>Arr[Node]) Then
+    If Arr[Root] < Arr[Child] Then
     Begin
-      Swap(Arr[Node], Arr[Child]);
-      Node:=Child;
+      Swap(Arr[Root], Arr[Child]);
+      Root:= Child;
     End
     Else
-      Sifted:=True;
-    Inc(Comp);
+      Sifted:= True;
   End;
-  Inc(Comp);
 End;
 
 Procedure HeapSort(Var Arr: TArr; Const Count: Integer; Var Comp: Integer);
 Var
-  NodeCurr, NodeLast: Integer;
-  Comps: Integer;
+  NodeLast, NodeCurr, Comps: Integer;
+  //NodeLast, NodeCurr - indexes
+  //Comps - counter
 Begin
-  NodeCurr:=Count Div 2;
-  While NodeCurr>1 Do
+  NodeCurr:= Count Div 2 - 1;
+  While NodeCurr >= 0 Do
   Begin
-    Comps:=0;
-    SiftDown(Arr, NodeCurr, NodeLast, Comps);
-    Comp:=Comp+Comps;
+    Comps:= 0;
+    SiftDown(Arr, NodeCurr, Count, Comps);
+    Comp:= Comp + Comps;
     Dec(NodeCurr);
   End;
-  NodeLast:=Count;
-  While NodeLast>1 Do
+  NodeLast:= Count;
+  While NodeLast > 0 Do
   Begin
-    Swap(Arr[1], Arr[NodeLast]);
-    Comps:=0;
-    SiftDown(Arr, NodeCurr, NodeLast, Comps);
-    Comp:=Comp+Comps;
+    Swap(Arr[0], Arr[NodeLast]);
+    Comps:= 0;
     Dec(NodeLast);
+    SiftDown(Arr, 0, NodeLast, Comps);
+    Comp:= Comp + Comps;
   End;
 End;
 
-Procedure GenInfo(Const N, Sort: Integer);
+Procedure Generate(Const N, Sort: Integer);
 Var
   Compare: Integer;
-  Arr, B: TArr;
+  Arr1, Arr2: TArr;
   Str: String;
+  //Compare - counter
+  //Arr1, Arr2 - similar arrays for the test
+  //Str - displaying
 Begin
 
+  //options for all cases
   Case Sort Of
     1:
       Begin
-        Str:='Random';
-        ArrR(Arr, N);
+        Str:= 'Random';
+        Fill(Arr1, N, Sort);
       End;
     2:
       Begin
-        Str:='Sorted';
-        ArrS(Arr, N);
+        Str:= 'Sorted';
+        Fill(Arr1, N, Sort);
       End;
     3:
       Begin
-        Str:='Vivers';
-        ArrVV(Arr, N);
+        Str:= 'Revers';
+        Fill(Arr1, N, Sort);
       End;
   End;
 
-  Write(Str, ' Arr[', N, ']; ');
+  //show arr size
+  Write(Str, ' Arr1[', N, ']; ');
 
-  B:=Arr;
+  //copy array
+  Arr2:= Arr1;
 
-  Compare:=0;
-  BubbleSort(Arr, N, Compare);
-  Write('Bubble: ', Compare: 7, '; ');
-
-  Compare:=0;
-  HeapSort(B, N, Compare);
-  Write('Heap: ', Compare: 5);
+  //enzero to show true value
+  Compare:= 0;
+  BubbleSort(Arr1, N, Compare);
+  Write('BubbleSort: ', Compare: 7, '; ');
+       
+  //enzero to show true value
+  Compare:= 0;
+  HeapSort(Arr2, N, Compare);
+  Write('HeapSort: ', Compare: 5);
 
   WriteLn;
 End;
 
 Begin
-  For I:=1 To 6 Do
+  For I:= 1 To 6 Do
   Begin
-    For J:=1 To 3 Do
-      GenInfo(Nums[I], J);
+    For J:= 1 To 3 Do
+      Generate(Nums[I], J);
     WriteLn;
   End;
   Readln;
 End.
+

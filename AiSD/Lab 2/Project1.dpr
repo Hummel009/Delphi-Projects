4eb05bd4ff5c1fd37b0a164014223dea68deadd6
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-03-15 21:26:21 +0300

    New lab

--- AiSD/Lab 2/Project1.dpr
+++ AiSD/Lab 2/Project1.dpr
@@ -1,184 +1,141 @@
-Program Lab1;
+Program Lab2;
 
-//Use app
 {$APPTYPE CONSOLE}
 
-//Declare modules
-Uses
-  SysUtils;
-
+//Declare types
 Type
-  //base type
-  TData = Integer;
-  //link to element
-  TPElem = ^TElem;
-  //the element
-  TElem = Record
-    //main data
-    Data:TData;
-    //link to the next element
-    PNext:TPElem;
+  //Two-sided list
+  TYesKGB = ^EYesKGB;
+  EYesKGB = Record
+    Data: String;
+    Next, Prev: TYesKGB;
   End;
-  //Order
-  TQueue = Record
-    //Num of elements
-    Cnt:Integer;
-    //Link to the first and the second
-    PFirst, PLast:TPElem;
-  End;   
-
+     
+  //One-sided list
+  TNoKGB = ^ENoKGB;
+  ENoKGB = Record
+    Data: String;
+    Next: TNoKGB;
+  End;
+  
+//Declare variables
 Var
-  Q:TQueue;
-  Data:TData;
-  Cmd, Code, i, Cnt, K, Kr:Integer;
-  S:String;
-  Loop:Boolean;
+  VYesKGB: TYesKGB;
+  VNoKGB: TNoKGB;
 
-//The creating (init) of the order. Only for not-empty (clear before)
-Procedure Init(Var aQ:TQueue);
-Begin
-  aQ.Cnt:= 0;
-  aQ.PFirst:= Nil;
-  aQ.PLast:= Nil;
-End;
-
-//Add to finish
-Procedure Push(Var aQ:TQueue; Const aData:TData);
+Procedure LoadYesKGB(Var X: TYesKGB);
 Var
-  PElem:TPElem;
+  I, Check, Error, Len: Integer;
+  Y: TYesKGB;
 Begin
-  New(PElem);
-  PElem^.Data:= aData;
-  PElem^.PNext:= Nil;
-  If aQ.PFirst = Nil Then
-    aQ.PFirst:= PElem
-  Else
-    aQ.PLast^.PNext:= PElem;
-  aQ.PLast:= PElem;
-  Inc(aQ.Cnt);
-End;
+  New(X);
+  Write('Enter the quantity of phone numbers: ');
+  ReadLn(Len);
+  X^.Prev:= Nil;
 
-//Take the first element. If is not empty, returns aData. If not, returns False and doesn't work
-Function Pop(Var aQ:TQueue; Var aData:TData):Boolean;
-Var
-  PElem:TPElem;
-Begin
-  Result:= False;
-  If aQ.PFirst = Nil Then Exit;
-
-  PElem:= aQ.PFirst;
-  aData:= PElem^.Data;
-  aQ.PFirst:= PElem^.PNext;
-  If aQ.PFirst = Nil Then
-    aQ.PLast:= Nil;
-  Dispose(PElem);
-  Dec(aQ.Cnt);
-  Result:= True;
-End;
-
-//Clear data
-Procedure Free(Var aQ:TQueue);
-Var
-  Data:TData;
-Begin
-  While Pop(aQ, Data) Do ;
-End;
-
-//Print from end to start
-Procedure QWriteln(Const aQ:TQueue);
-Var
-  PElem:TPElem;
-  i:Integer;
-Begin
-  PElem:= aQ.PFirst;
-  i:= 0;
-  While PElem <> Nil Do
+  //Filling the list
+  For I:= 0 To Len - 1 Do
   Begin
-    Inc(i);
-    If i > 1 Then Write(', ');
-    Write(PElem^.Data);
-    PElem:= PElem^.PNext;
-  End;
-  Writeln;
-End;
+    Y:= X;
 
-Begin
-  Init(Q);
-
-  Write('The loop? Type "Yes" or "No": ');
-  Readln(S);
-  If S = 'Yes' Then
-    loop:= true
-  Else
-    loop:= False;
-
-  If Not loop Then
-    //Enter quantity
+    //Check for the correct input
     Repeat
-      Write('Quantity: ');
-      Readln(S);
-      Val(S, Cnt, Code);
-      If (Code > 0) Or (Cnt <= 0) Then
-      Begin
-        Writeln('Error.');
-        Code:= 1;
-      End
-    Until Code = 0;
+      Write('Number #', I+1, ':');
+      ReadLn(X^.Data);
+      Val(X^.Data, Check, Error);
+      If ((Length(X^.Data) <> 7) And (Length(X^.Data) <> 3)) Or (Error <> 0) Then
+        WriteLn('Incorrect input');
+    Until ((Length(X^.Data) = 7) Or (Length(X^.Data) = 3)) And (Error = 0);
 
-  //Enter the number of disappearing person
-  Repeat
-    Write('Disappearing: ');
-    Readln(S);
-    Val(S, K, Code);
-    If (Code > 0) Or (K <= 0) Then
+    //Setting previous and next for every element
+    If I < Len - 1 Then
     Begin
-      Writeln('Error.');
-      Code:= 1;
-    End
-  Until Code = 0;
-
-  //Clear memory
-  Free(Q);
-  For Cnt:= 1 To 64 Do
-  Begin
-    //Add users to list
-    For i:= 1 To Cnt Do
-      Push(Q, i);
-
-    //Repeat until there is one alive
-    While Q.Cnt > 1 Do
-    Begin
-      //The pos of disappearing person
-      Kr:= K Mod Q.Cnt;
-      If Kr = 0 Then Kr:= Q.Cnt;
-
-      //move all people BEFORE disappearing from the beginning to end
-      For i:= 1 To Kr - 1 Do
-      Begin
-        //From the beginning
-        Pop(Q, Data);
-        //To the end
-        Push(Q, Data);
-      End;
-
-      //The current first person is KR. Remove and do not add back
-      Pop(Q, Data);
-
-      //Display him
-      Write(Data, ' ');
+      New(X);
+      Y^.Next:= X;
+      X^.Prev:= Y;
     End;
-    Pop(Q, Data);
-
-    //The last person is alive
-    Write('| Alive: ', Data);
-
-    //Clear memory
-    Free(Q);
-    Writeln;
-    Writeln;
   End;
+  Y^.Next:= Nil;
+End;
 
-  Readln;
+//Shows all numbers including KGB
+Procedure PrintYesKGB(X: TYesKGB);
+Begin
+  While X <> Nil Do
+  Begin
+    WriteLn(X^.Data);
+    X:= X^.Prev;
+  End;
+End;
+  
+//Shows all numbers excluding KGB
+Procedure PrintNoKGB(X: TNoKGB);
+Begin
+  While X <> Nil Do
+  Begin
+    WriteLn(X^.Data);
+    X:= X^.Next;
+  End;
+End;
+             
+//Removes KGB numbers (3 symbols) from the list
+Procedure RemoveKGB(Y: TNoKGB; X: TYesKGB);
+Begin
+  While X <> Nil Do
+  Begin
+    If Length(X^.Data) <> 3 Then
+    Begin
+      New(Y^.Next);
+      Y:= Y^.Next;
+      Y^.Data:= X^.Data;
+    End;
+    X:= X^.Prev;
+  End;
+  Y^.Next:= Nil;
+End;
+
+//Sorts not-KGB numbers by symbols
+Procedure SortNoKGB(Start: TNoKGB);
+Var
+  I, J, Len: Integer;
+  X: TNoKGB;
+  Temp: String;
+Begin
+  X:= Start;
+  Len:= 0;
+  While X <> Nil Do
+  Begin
+    Inc(Len);
+    X:= X^.Next;
+  End;
+  For I:= 1 To Len Do
+  Begin
+    X:= Start;
+
+    //Comparing strings does the same as comparing every number
+    For J:= 1 To Len - I Do
+    Begin
+      If X^.Data > X^.Next^.Data Then
+      Begin
+        Temp:= X^.Data;
+        X^.Data:= X^.Next^.Data;
+        X^.Next^.Data:= Temp;
+      End;
+      X:= X^.Next;
+    End;
+  End;
+End;
+
+Begin
+  LoadYesKGB(VYesKGB);
+  WriteLn;
+  WriteLn('Phone numbers including KGB:');
+  PrintYesKGB(VYesKGB);
+  WriteLn;
+  Write('Phone numbers excluding KGB:');    
+  New(VNoKGB);
+  RemoveKGB(VNoKGB, VYesKGB);
+  SortNoKGB(VNoKGB);
+  PrintNoKGB(VNoKGB);
+  ReadLn;
 End.
-
-

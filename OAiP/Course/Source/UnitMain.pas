40c43106203f1037044a55ca602ec2ff4c71cc5f
Hummel009 <emperorbur@gmail.com>
2022-05-20 21:03:44 +0300

    Наверно, это финал

--- OAiP/Course/Source/UnitMain.pas
+++ OAiP/Course/Source/UnitMain.pas
@@ -1,4 +1,5 @@
 Unit UnitMain;
+{This program calculates ops on numbers}
 
 Interface
 
@@ -17,19 +18,28 @@ Uses
   Menus;
 
 Type
+  //List of ops
   TOp = (ENULL, EARCCOS, EARCCTG, EARCSIN, EARCTG, ECOS, ECTG, EDIVIDE, EFACTORIAL, EMINUS, EMULTIPLE, EPERCENT, EPLUS, EPOWER, ESIN, ESQRT, ETG, ESQUARE, ECUBE, ELG, ELN, ECH, ESH, ETH, ECTH, ETEN, EBACK, EDFACTORIAL, EEXP, ETWO, ESC, ECSC, EARCSC, EARCCSC, ESCH, ECSCH, EVERSIN, EVERCOS, EHAVERSIN, EHAVERCOS, EEXSC, EEXCSC);
+
+  //Linked list
   TLine = ^ELine;
   ELine = Record
     Data: Real;
     Next, Prev: TLine;
   End;
+
+  //Record of data in orig types
   TMem = Record
     Inp1, Inp2, Res: Real;
     Op: TOp;
   End;
+
+  //Record of data in string view
   TDisp = Record
     Inp1, Inp2, Res, Op, Disp: String[50];
   End;
+
+  //GUI elements, procedures
   TFormMain = Class(TForm)
     btn1: TButton;
     btn2: TButton;
@@ -71,10 +81,63 @@ Type
     btnFloat: TButton;
     dlgOpen: TOpenDialog;
     lblRes: TLabel;
-    Procedure btnFloatClick(Sender: TObject);
+
+    //This proc cals res depending on op
+    //FRes - result
+    Procedure Calculate(Var FRes: Real);
+
+    //This proc converts str to float and resets some flags
+    //FInp - input, FRes - result
+    Procedure ConvertSF(Var FInp: String; Var FRes: Real);
+
+    //This proc displays res
+    Procedure Display();
+
+    //This proc calcs factorials
+    //FOp - factorial, FInt - error border
+    Procedure FactException(Const FOp: TOp; Const FInt: Integer);
+
+    //This proc calcs one-operand ops
+    //FOp - operation, FInt - error border
+    Procedure LimException(Const FOp: TOp; Const FInt: Extended);
+
+    //This proc calcs one-operand ops
+    //FOp - operation
+    Procedure NegException(Const FOp: TOp);
+
+    //This proc calcs one-operand ops
+    //FOp - operation
+    Procedure NoException(Const FOp: TOp);
+
+    //This proc calcs one-operand ops
+    //FOp - operation
+    Procedure OneException(Const FOp: TOp);
+
+    //This proc puts sym on the screen
+    //FSym - symbol
+    Procedure PlaceSymbol(Const FSym: String);
+
+    //This proc clears data
+    //FMem - data
+    Procedure ResetData(Var FMem: TMem);
+
+    //This proc saves data into file
+    Procedure SaveData();
+
+    //This proc calcs two-operand ops
+    //FOp - operation
+    Procedure TwoNumbers(Const FOp: TOp);
+
+    //This proc calcs one-operand ops
+    //FOp - operation
+    Procedure ZeroException(Const FOp: TOp);
+
+    //GUI procedures
     Procedure btn0Click(Sender: TObject);
+    Procedure btn10PowerClick(Sender: TObject);
     Procedure btn1Click(Sender: TObject);
     Procedure btn2Click(Sender: TObject);
+    Procedure btn2PowerClick(Sender: TObject);
     Procedure btn3Click(Sender: TObject);
     Procedure btn4Click(Sender: TObject);
     Procedure btn5Click(Sender: TObject);
@@ -82,42 +145,32 @@ Type
     Procedure btn7Click(Sender: TObject);
     Procedure btn8Click(Sender: TObject);
     Procedure btn9Click(Sender: TObject);
-    Procedure btnDotClick(Sender: TObject);
+    Procedure btnBackClick(Sender: TObject);
+    Procedure btnBulkClick(Sender: TObject);
     Procedure btnClearAllClick(Sender: TObject);
     Procedure btnClearClick(Sender: TObject);
-    Procedure btnEilerClick(Sender: TObject);
-    Procedure btnPeeClick(Sender: TObject);
-    Procedure btnPlusClick(Sender: TObject);
-    Procedure btnEqClick(Sender: TObject);
-    Procedure btnMultipleClick(Sender: TObject);
-    Procedure btnDivideClick(Sender: TObject);
-    Procedure btnMinusClick(Sender: TObject);
-    Procedure btnSqrtClick(Sender: TObject);
-    Procedure btnPowerYClick(Sender: TObject);
-    Procedure btnPower2Click(Sender: TObject);
-    Procedure btnPosNegClick(Sender: TObject);
-    Procedure btnBackClick(Sender: TObject);
-    Procedure btnFactorialClick(Sender: TObject);
     Procedure btnDFactorialClick(Sender: TObject);
-    Procedure btnPercentClick(Sender: TObject);
-    Procedure btn10PowerClick(Sender: TObject);
+    Procedure btnDivideClick(Sender: TObject);
+    Procedure btnDotClick(Sender: TObject);
+    Procedure btnEilerClick(Sender: TObject);
+    Procedure btnEqClick(Sender: TObject);
+    Procedure btnExpClick(Sender: TObject);
+    Procedure btnFactorialClick(Sender: TObject);
+    Procedure btnFloatClick(Sender: TObject);
+    Procedure btnHistClick(Sender: TObject);
     Procedure btnLgClick(Sender: TObject);
     Procedure btnLnClick(Sender: TObject);
+    Procedure btnMinusClick(Sender: TObject);
+    Procedure btnMultipleClick(Sender: TObject);
+    Procedure btnPeeClick(Sender: TObject);
+    Procedure btnPercentClick(Sender: TObject);
+    Procedure btnPlusClick(Sender: TObject);
+    Procedure btnPosNegClick(Sender: TObject);
+    Procedure btnPower2Click(Sender: TObject);
     Procedure btnPower3Click(Sender: TObject);
-    Procedure btn2PowerClick(Sender: TObject);
-    Procedure btnExpClick(Sender: TObject);
-    Procedure btnHistClick(Sender: TObject);
+    Procedure btnPowerYClick(Sender: TObject);
+    Procedure btnSqrtClick(Sender: TObject);
     Procedure btnTrigClick(Sender: TObject);
-    Procedure btnBulkClick(Sender: TObject);
-    Procedure NoException(Const FOp: TOp);
-    Procedure OneException(Const FOp: TOp);
-    Procedure ZeroException(Const FOp: TOp);
-    Procedure LimException(Const FOp: TOp; Const FInt: Extended);
-    Procedure FactException(Const FOp: TOp; Const FInt: Integer);
-    Procedure NegException(Const FOp: TOp);
-    Procedure PlaceSymbol(Const FSym: String);
-    Procedure TwoNumbers(Const FOp: TOp);
-    Procedure Display();
   Private
     { Private declarations }
   Public
@@ -125,19 +178,35 @@ Type
   End;
 
 Var
+  //Eiler num
   E: Real = 2.718281828459045;
+
+  //Pe num
   P: Real = 3.141592653589793;
+
+  //String view of ops for the history
   GOpView: Array[TOp] Of String = ('?', 'arccos', 'arcctg', 'arcsin', 'arctg', 'cos', 'ctg', '/', '!', '-', '*', '%', '+', '', 'sin', 'sqrt', 'tg', ' 2', ' 3', 'lg', 'ln', 'ch', 'sh', 'th', 'cth', '10 ', '1 /', '!!', 'exp', '2 ', 'sec', 'cosec', 'arcsec', 'arccosec', 'sch', 'csch', 'versin', 'vercos', 'haversin', 'havercos', 'exsec', 'excosec');
+
+  //Best factorial implementation - (c) Onoshko
   GFact1: Array[0..12] Of Integer = (1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600);
   GFact2: Array[0..19] Of Integer = (1, 1, 2, 3, 8, 15, 48, 105, 384, 945, 3840, 10395, 46080, 135135, 645120, 2027025, 10321920, 34459425, 185794560, 654729075);
+
+  //Categories of ops to be shown in right way in the hist
   GHist1: Set Of TOp = [EARCCOS, EARCCTG, EARCSIN, EARCTG, ECOS, ECTG, ESIN, ESQRT, ETG, ELG, ELN, ECH, ESH, ETH, ECTH, EEXP, ESC, ECSC, EARCSC, EARCCSC, ESCH, ECSCH, EVERSIN, EVERCOS, EHAVERSIN, EHAVERCOS, EEXSC, EEXCSC];
   GHist2: Set Of TOp = [EFACTORIAL, EDFACTORIAL, ESQUARE, ECUBE];
   GHist3: Set Of TOp = [EBACK, ETEN, ETWO];
-  FormMain: TFormMain;
+
+  //Memory in string view - operands and op
   GDisp: TDisp;
+
+  //Memory in orig view - operands and op
   GMem: TMem;
-  GLine: Integer;
+
+  //Mistake & clean flags
   GError, GClear: Boolean;
+
+  //GUI
+  FormMain: TFormMain;
 
 Implementation
 
@@ -145,7 +214,7 @@ Uses UnitHist,
   UnitTrig;
 {$R *.dfm}
 
-Procedure Calculate(Var FRes: Real);
+Procedure TFormMain.Calculate(Var FRes: Real);
 Begin
   Case GMem.Op Of
     EPLUS:
@@ -234,75 +303,7 @@ Begin
   End;
 End;
 
-Procedure ResetData(Var FMem: TMem);
-Begin
-  GMem.Op:= ENULL;
-  GError:= False;
-  With FMem Do
-  Begin
-    Inp1:= 0;
-    Inp2:= 0;
-    Res:= 0;
-  End;
-End;
-
-Procedure SaveData();
-Var
-  LFile: File Of TDisp;
-Begin
-  If GMem.Op <> ENULL Then
-  Begin
-    GDisp.Op:= GOpView[GMem.Op];
-    GDisp.Inp1:= FloatToStr(GMem.Inp1);
-    GDisp.Inp2:= FloatToStr(GMem.Inp2);
-    GDisp.Res:= FloatToStr(GMem.Res);
-
-    If GMem.Op In GHist1 Then
-      GDisp.Disp:= GDisp.Op + '(' + GDisp.Inp1 + ') = ' + GDisp.Res
-    Else
-      If GMem.Op In GHist2 Then
-        GDisp.Disp:= GDisp.Inp1 + ' ' + GDisp.Op + ' = ' + GDisp.Res
-      Else
-        If GMem.Op In GHist3 Then
-          GDisp.Disp:= GDisp.Op + ' ' + GDisp.Inp1 + ' = ' + GDisp.Res
-        Else
-          GDisp.Disp:= GDisp.Inp1 + ' ' + GDisp.Op + ' ' + GDisp.Inp2 + ' = ' + GDisp.Res;
-
-    AssignFile(LFile, 'Hummel009.hzzn');
-
-    If Not FileExists('Hummel009.hzzn') Then
-      Rewrite(LFile);
-
-    Reset(LFile);
-    Seek(LFile, FileSize(LFile));
-    Write(LFile, GDisp);
-    CloseFile(LFile);
-  End;
-
-  GClear:= True;
-End;
-
-Procedure TFormMain.Display();
-Var
-  TRes: Real;
-Begin
-  If ((GMem.Op = EPOWER) And ((GMem.Inp1 > 143) Or (GMem.Inp2 > 143))) And ((GMem.Op = EDIVIDE) And (GMem.Inp2 = 0)) Then
-    GError:= True;
-
-  If GMem.Op <> ENULL Then
-    If (Not GError) Then
-    Begin
-      Calculate(TRes);
-      GMem.Res:= TRes;
-      lblRes.Caption:= FloatToStr(GMem.Res);
-      SaveData();
-    End
-    Else
-      lblRes.Caption:= 'Error. No correct input';
-  ResetData(GMem);
-End;
-
-Procedure ConvertSF(Var FInp: String; Var FRes: Real);
+Procedure TFormMain.ConvertSF(Var FInp: String; Var FRes: Real);
 Var
   LPos, LLim: Integer;
   LPower: String;
@@ -324,47 +325,24 @@ Begin
   End;
 End;
 
-Procedure TFormMain.btnEqClick(Sender: TObject);
+Procedure TFormMain.Display();
 Var
-  LInp: String;
+  TRes: Real;
 Begin
-  LInp:= lblRes.Caption;
-  ConvertSF(LInp, GMem.Inp2);
-  Display();
-End;
-
-Procedure TFormMain.ZeroException(Const FOp: TOp);
-Var
-  LInp: String;
-Begin
-  LInp:= lblRes.Caption;
-  GMem.Op:= FOp;
-  ConvertSF(LInp, GMem.Inp1);
-  If (GMem.Inp1 = 0) Then
+  If ((GMem.Op = EPOWER) And ((GMem.Inp1 > 143) Or (GMem.Inp2 > 143))) And ((GMem.Op = EDIVIDE) And (GMem.Inp2 = 0)) Then
     GError:= True;
-  Display();
-End;
 
-Procedure TFormMain.OneException(Const FOp: TOp);
-Var
-  LInp: String;
-Begin
-  LInp:= lblRes.Caption;
-  GMem.Op:= FOp;
-  ConvertSF(LInp, GMem.Inp1);
-  If (GMem.Inp1 > 1) Or (GMem.Inp1 < -1) Then
-    GError:= True;
-  Display();
-End;
-
-Procedure TFormMain.NoException(Const FOp: TOp);
-Var
-  LInp: String;
-Begin
-  LInp:= lblRes.Caption;
-  GMem.Op:= FOp;
-  ConvertSF(LInp, GMem.Inp1);
-  Display();
+  If GMem.Op <> ENULL Then
+    If (Not GError) Then
+    Begin
+      Calculate(TRes);
+      GMem.Res:= TRes;
+      lblRes.Caption:= FloatToStr(GMem.Res);
+      SaveData();
+    End
+    Else
+      lblRes.Caption:= 'Error. No correct input';
+  ResetData(GMem);
 End;
 
 Procedure TFormMain.FactException(Const FOp: TOp; Const FInt: Integer);
@@ -403,14 +381,26 @@ Begin
   Display();
 End;
 
-Procedure TFormMain.TwoNumbers(Const FOp: TOp);
+Procedure TFormMain.NoException(Const FOp: TOp);
 Var
   LInp: String;
 Begin
   LInp:= lblRes.Caption;
   GMem.Op:= FOp;
   ConvertSF(LInp, GMem.Inp1);
-  lblRes.Caption:= '';
+  Display();
+End;
+
+Procedure TFormMain.OneException(Const FOp: TOp);
+Var
+  LInp: String;
+Begin
+  LInp:= lblRes.Caption;
+  GMem.Op:= FOp;
+  ConvertSF(LInp, GMem.Inp1);
+  If (GMem.Inp1 > 1) Or (GMem.Inp1 < -1) Then
+    GError:= True;
+  Display();
 End;
 
 Procedure TFormMain.PlaceSymbol(Const FSym: String);
@@ -454,118 +444,74 @@ Begin
   lblRes.Caption:= LInp + LAdd;
 End;
 
-Procedure TFormMain.btnClearClick(Sender: TObject);
-Var
-  LInp: String;
-  LLen: Integer;
+Procedure TFormMain.ResetData(Var FMem: TMem);
 Begin
-  If (lblRes.Caption = 'Error. No correct input') Then
-    lblRes.Caption:= '0'
-  Else
-  Begin
-    LInp:= lblRes.Caption;
-    LLen:= Length(LInp);
-    Delete(LInp, LLen, 1);
-    lblRes.Caption:= LInp;
-    If (LLen = 1) Then
-      lblRes.Caption:= '0';
-  End;
-End;
-
-Procedure TFormMain.btnHistClick(Sender: TObject);
-Var
-  LFile: File Of TDisp;
-  LLine: TDisp;
-Begin
-  FormHist.mmoHistory.Lines.Clear;
-
-  AssignFile(LFile, 'Hummel009.hzzn');
-  If Not FileExists('Hummel009.hzzn') Then
-    Rewrite(LFile);
-
-  Reset(LFile);
-
-  GLine:= 0;
-  While Not Eof(LFile) Do
-  Begin
-    Read(LFile, LLine);
-    FormHist.mmoHistory.Lines.Insert(GLine, LLine.Disp);
-    Inc(GLine);
-  End;
-
-  FormHist.show;
-End;
-
-Procedure TFormMain.btnBulkClick(Sender: TObject);
-Var
-  LFile: TextFile;
-  LData: String;
-  LLine1, LLine2: TLine;
-  LRes: Real;
-Begin
+  GMem.Op:= ENULL;
   GError:= False;
-  With GMem Do
+  With FMem Do
   Begin
     Inp1:= 0;
     Inp2:= 0;
     Res:= 0;
   End;
+End;
 
-  If dlgOpen.Execute Then
+Procedure TFormMain.SaveData();
+Var
+  LFile: File Of TDisp;
+Begin
+  If GMem.Op <> ENULL Then
   Begin
-    AssignFile(LFile, dlgOpen.FileName);
+    GDisp.Op:= GOpView[GMem.Op];
+    GDisp.Inp1:= FloatToStr(GMem.Inp1);
+    GDisp.Inp2:= FloatToStr(GMem.Inp2);
+    GDisp.Res:= FloatToStr(GMem.Res);
+
+    If GMem.Op In GHist1 Then
+      GDisp.Disp:= GDisp.Op + '(' + GDisp.Inp1 + ') = ' + GDisp.Res
+    Else
+      If GMem.Op In GHist2 Then
+        GDisp.Disp:= GDisp.Inp1 + ' ' + GDisp.Op + ' = ' + GDisp.Res
+      Else
+        If GMem.Op In GHist3 Then
+          GDisp.Disp:= GDisp.Op + ' ' + GDisp.Inp1 + ' = ' + GDisp.Res
+        Else
+          GDisp.Disp:= GDisp.Inp1 + ' ' + GDisp.Op + ' ' + GDisp.Inp2 + ' = ' + GDisp.Res;
+
+    AssignFile(LFile, 'Hummel009.hzzn');
+
+    If Not FileExists('Hummel009.hzzn') Then
+      Rewrite(LFile);
+
     Reset(LFile);
-
-    New(LLine1);
-    LLine1^.Prev:= Nil;
-    While Not EoF(LFile) Do
-    Begin
-      LLine2:= LLine1;
-      ReadLn(LFile, LData);
-      ConvertSF(LData, LLine1^.Data);
-      New(LLine1);
-      LLine2^.Next:= LLine1;
-      LLine1^.Prev:= LLine2;
-    End;
-    LLine2^.Next:= Nil;
-
-    //One step back
-    LLine1:= LLine1^.Prev;
-    LLine1^.Next:= Nil;
-
+    Seek(LFile, FileSize(LFile));
+    Write(LFile, GDisp);
     CloseFile(LFile);
-
-    Case GMem.Op Of
-      EPLUS:
-        Begin
-          LRes:= 0;
-          While LLine1 <> Nil Do
-          Begin
-            LRes:= LRes + LLine1^.Data;
-            LLine1:= LLine1^.Prev;
-          End;
-        End;
-      EMULTIPLE:
-        Begin
-          LRes:= 1;
-          While LLine1 <> Nil Do
-          Begin
-            LRes:= LRes * LLine1^.Data;
-            LLine1:= LLine1^.Prev;
-          End;
-        End;
-    Else
-      GError:= True;
-      LRes:= 0;
-    End;
-
-    If Not GError Then
-      lblRes.Caption:= FloatToStr(LRes)
-    Else
-      lblRes.Caption:= 'Error. No correct input';
-    Dispose(LLine1);
-    ResetData(GMem);
   End;
+
+  GClear:= True;
+End;
+
+Procedure TFormMain.TwoNumbers(Const FOp: TOp);
+Var
+  LInp: String;
+Begin
+  LInp:= lblRes.Caption;
+  GMem.Op:= FOp;
+  ConvertSF(LInp, GMem.Inp1);
+  lblRes.Caption:= '';
+End;
+
+Procedure TFormMain.ZeroException(Const FOp: TOp);
+Var
+  LInp: String;
+Begin
+  LInp:= lblRes.Caption;
+  GMem.Op:= FOp;
+  ConvertSF(LInp, GMem.Inp1);
+  If (GMem.Inp1 = 0) Then
+    GError:= True;
+  Display();
 End;
 
 Procedure TFormMain.btnBackClick(Sender: TObject);
@@ -596,6 +542,15 @@ End;
 Procedure TFormMain.btn2PowerClick(Sender: TObject);
 Begin
   LimException(ETWO, 1024);
+End;
+
+Procedure TFormMain.btnEqClick(Sender: TObject);
+Var
+  LInp: String;
+Begin
+  LInp:= lblRes.Caption;
+  ConvertSF(LInp, GMem.Inp2);
+  Display();
 End;
 
 Procedure TFormMain.btn10PowerClick(Sender: TObject);
@@ -737,6 +692,121 @@ End;
 Procedure TFormMain.btnTrigClick(Sender: TObject);
 Begin
   FormTrig.show;
+End;
+
+Procedure TFormMain.btnClearClick(Sender: TObject);
+Var
+  LInp: String;
+  LLen: Integer;
+Begin
+  If (lblRes.Caption = 'Error. No correct input') Then
+    lblRes.Caption:= '0'
+  Else
+  Begin
+    LInp:= lblRes.Caption;
+    LLen:= Length(LInp);
+    Delete(LInp, LLen, 1);
+    lblRes.Caption:= LInp;
+    If (LLen = 1) Then
+      lblRes.Caption:= '0';
+  End;
+End;
+
+Procedure TFormMain.btnHistClick(Sender: TObject);
+Var
+  LFile: File Of TDisp;
+  LLine: TDisp;
+  LInsert: Integer;
+Begin
+  FormHist.mmoHistory.Lines.Clear;
+
+  AssignFile(LFile, 'Hummel009.hzzn');
+  If Not FileExists('Hummel009.hzzn') Then
+    Rewrite(LFile);
+
+  Reset(LFile);
+
+  LInsert:= 0;
+  While Not Eof(LFile) Do
+  Begin
+    Read(LFile, LLine);
+    FormHist.mmoHistory.Lines.Insert(LInsert, LLine.Disp);
+    Inc(LInsert);
+  End;
+
+  FormHist.show;
+End;
+
+Procedure TFormMain.btnBulkClick(Sender: TObject);
+Var
+  LFile: TextFile;
+  LData: String;
+  LLine1, LLine2: TLine;
+  LRes: Real;
+Begin
+  GError:= False;
+  With GMem Do
+  Begin
+    Inp1:= 0;
+    Inp2:= 0;
+    Res:= 0;
+  End;
+
+  If dlgOpen.Execute Then
+  Begin
+    AssignFile(LFile, dlgOpen.FileName);
+    Reset(LFile);
+
+    New(LLine1);
+    LLine1^.Prev:= Nil;
+    While Not EoF(LFile) Do
+    Begin
+      LLine2:= LLine1;
+      ReadLn(LFile, LData);
+      ConvertSF(LData, LLine1^.Data);
+      New(LLine1);
+      LLine2^.Next:= LLine1;
+      LLine1^.Prev:= LLine2;
+    End;
+    LLine2^.Next:= Nil;
+
+    //One step back
+    LLine1:= LLine1^.Prev;
+    LLine1^.Next:= Nil;
+
+    CloseFile(LFile);
+
+    Case GMem.Op Of
+      EPLUS:
+        Begin
+          LRes:= 0;
+          While LLine1 <> Nil Do
+          Begin
+            LRes:= LRes + LLine1^.Data;
+            LLine1:= LLine1^.Prev;
+          End;
+        End;
+      EMULTIPLE:
+        Begin
+          LRes:= 1;
+          While LLine1 <> Nil Do
+          Begin
+            LRes:= LRes * LLine1^.Data;
+            LLine1:= LLine1^.Prev;
+          End;
+        End;
+    Else
+      GError:= True;
+      LRes:= 0;
+    End;
+
+    If Not GError Then
+      lblRes.Caption:= FloatToStr(LRes)
+    Else
+      lblRes.Caption:= 'Error. No correct input';
+    Dispose(LLine1);
+    ResetData(GMem);
+  End;
 End;
 
 Initialization
